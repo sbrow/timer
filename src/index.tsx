cae937196cd39f647ee2d0fc2737a0098e2f8d23
Spencer Brower <brower.spencer@gmail.com>
2019-01-21 18:19:07 -0500

    * Properly ignoring node_modules/ * No longer opens browser when launching electron. * Fixed some typing issues.

--- src/index.tsx
+++ src/index.tsx
@@ -1,48 +1,63 @@
-import * as React from 'react' 
-import * as ReactDOM from 'react-dom' 
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
 
 type Millisecond = number;
 
 const hour: Millisecond = 3600000
 const precision: number = 4
 
-class Timer extends React.Component<any, any> {
-    interval: any
+interface timerProperties {
+    interval: Millisecond
+}
 
-    constructor(props: any){
+interface timerState {
+    time: Millisecond
+}
+
+
+class Timer extends React.Component<any, any> {
+    // Both of these properties are inherited from React.Component
+
+    // TODO: fix Timer.interval to show correct type
+    interval: NodeJS.Timeout | undefined
+    state: timerState
+
+    constructor(props: timerProperties) {
         super(props)
         this.state = {
             time: 0,
         }
     }
-/*     start(): void {
-        this.id = setInterval(()=>this.tick(), 100)
-    } */
+    /*     start(): void {
+            this.id = setInterval(()=>this.tick(), 100)
+        } */
     tick(): void {
         this.setState({
-            time: this.state.time + 100,
-          })
+            time: this.state.time + this.props.interval
+        })
     }
     componentDidMount() {
-        this.interval = setInterval(() => this.tick(), 100);
+        this.interval = setInterval(() => this.tick(), this.props.interval);
     }
-    
+
     componentWillUnmount() {
-        clearInterval(this.interval);
+        if (this.interval !== undefined) {
+            clearInterval(this.interval);
+        }
     }
     hours(): string {
-        return (this.state.time/hour).toFixed(precision)
+        return (this.state.time / hour).toFixed(precision)
     }
     render(): any {
         return (
             <div>
-              Hours: {this.hours()}
+                Hours: {this.hours()}
             </div>
-          );
+        );
     }
 }
 
 ReactDOM.render(
-    <Timer interval="100" />,
+    <Timer interval={100} />,
     document.getElementById('timer-example')
-  );
+);

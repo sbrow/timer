f27351b4e1b0cba494dcf5b4f7e2b6fc735323d5
Spencer Brower <foxtrotalphalima@gmail.com>
2019-03-06 18:26:53 -0500

    Time is now calculated as `time.now() - time.started` instead of being added to each tick.

--- app/stores/TimerStore.ts
+++ app/stores/TimerStore.ts
@@ -9,6 +9,7 @@ import { Action, Millisecond } from "../global"
 export class TimerStore extends EventEmitter {
   public dispatcher: Dispatcher<Action>
   protected time: Millisecond
+  protected started: Millisecond | null
   protected step: Millisecond
   protected interval: NodeJS.Timeout | null
 
@@ -16,15 +17,20 @@ export class TimerStore extends EventEmitter {
     super()
     this.dispatcher = dispatcher
     this.time = 0
+    this.started = null
     this.step = 360
     this.interval = null
   }
 
   public getState(): TCState {
+    let time = this.time
+    if (this.started !== null) {
+      time += Date.now() - this.started
+    }
     return {
       on: (this.interval !== null),
       step: this.step,
-      time: this.time,
+      time: time
     }
   }
 
@@ -53,7 +59,6 @@ export class TimerStore extends EventEmitter {
    * @memberof TimerStore
    */
   protected tick(): void {
-    this.time += this.step
     this.emit("change")
   }
 
@@ -61,24 +66,37 @@ export class TimerStore extends EventEmitter {
     // Don't start if already started.
     if (this.interval === null) {
       this.interval = setInterval(() => { this.dispatcher.dispatch(TickAction) }, this.step)
+      if (this.started === null) {
+        this.started = Date.now()
+      }
       this.emit("change")
     }
   }
 
   protected stop = (): void => {
     // Don't stop if timer not running.
-    if (this.interval) {
+    if (this.interval !== null) {
       clearInterval(this.interval)
       this.interval = null
+      if (this.started !== null) {
+        this.time += Date.now() - this.started
+      }
+      this.started = null
       this.emit("change")
     }
   }
 
   protected clear = (): void => {
     // Don't emit change if store is already clear.
-    if (this.time !== 0) {
-      this.time = 0
-      this.emit("change")
+    if (this.time === 0 && this.started === null) {
+      return
     }
+    this.time = 0
+    if (this.interval === null) {
+      this.started = null
+    } else {
+      this.started = Date.now()
+    }
+    this.emit("change")
   }
 }
